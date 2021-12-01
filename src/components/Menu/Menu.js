10ea3e4a5c1e366658214a97280f464b38667797
IlliaLisitsa <liyalisica9356@gmail.com>
2021-12-01 20:15:02 +0300

    change menu design and add shadow

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet, SafeAreaView } from 'react-native';
+import { View, StyleSheet, SafeAreaView, Platform } from 'react-native';
 import { PostsIconNotActive } from '../../../assets/componentIcons/posts/PostsIconNotActive';
 import { UsersIconNotActive } from '../../../assets/componentIcons/users/UsersIconNotActive';
 import { AddPostIconNotActive } from '../../../assets/componentIcons/addPost/AddPostIconNotActive';
@@ -21,11 +21,25 @@ export const Menu = () => {
 const styles = StyleSheet.create({
     container: {
         position: 'absolute',
-        bottom: 25,
+        bottom: 0,
         alignItems: 'center',
         width: '100%',
+        backgroundColor: '#FFFFFF',
+        paddingBottom: 10,
+        borderTopLeftRadius: 25,
+        borderTopRightRadius: 25,
+        borderColor: 'transparent',
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 0,
+        },
+        shadowOpacity: 0.05,
+        shadowRadius: 10,
+        elevation: Platform.OS === 'ios' ? 15 : 25,
     },
     wrapper: {
+        paddingTop: 10,
         flexDirection: 'row',
         width: '80%',
         alignItems: 'center',
