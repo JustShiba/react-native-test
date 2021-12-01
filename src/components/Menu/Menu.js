0f1194da1fb7969bb37827ce3991c46c8bd11bec
IlliaLisitsa <liyalisica9356@gmail.com>
2021-12-01 18:47:44 +0300

    add icons in menu

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,29 +1,34 @@
 import React from 'react';
-import {
-    Text,
-    View,
-    StyleSheet,
-    TextInput,
-    KeyboardAvoidingView,
-    SafeAreaView,
-} from 'react-native';
+import { View, StyleSheet, SafeAreaView } from 'react-native';
+import { PostsIconNotActive } from '../../../assets/componentIcons/posts/PostsIconNotActive';
+import { UsersIconNotActive } from '../../../assets/componentIcons/users/UsersIconNotActive';
+import { AddPostIconNotActive } from '../../../assets/componentIcons/addPost/AddPostIconNotActive';
+import { HomeIconActive } from '../../../assets/componentIcons/home/HomeIconActive';
 
 export const Menu = () => {
     return (
-        <KeyboardAvoidingView style={styles.container} behavior="padding">
-            <SafeAreaView>
-                <TextInput />
-                <Text>Hello Menu!</Text>
-            </SafeAreaView>
-        </KeyboardAvoidingView>
+        <SafeAreaView style={styles.container}>
+            <View style={styles.wrapper}>
+                <PostsIconNotActive />
+                <UsersIconNotActive />
+                <AddPostIconNotActive />
+                <HomeIconActive />
+            </View>
+        </SafeAreaView>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
-        // position: 'absolute',
-        // bottom: 0,
-        marginTop: 'auto',
-        marginBottom: 15,
+        position: 'absolute',
+        bottom: 25,
+        alignItems: 'center',
+        width: '100%',
+    },
+    wrapper: {
+        flexDirection: 'row',
+        width: '80%',
+        alignItems: 'center',
+        justifyContent: 'space-around',
     },
 });
