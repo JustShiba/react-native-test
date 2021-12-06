5cb80c02ee6e7b9b7533e16badce51ada879be02
IlliaLisitsa <liyalisica9356@gmail.com>
2021-12-06 18:07:32 +0300

    start creating addPostPage

---
+++ src/pages/AddPostPage/AddPostPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export const AddPostPage = () => {
+    return (
+        <View>
+            <Text>Hello new post</Text>
+        </View>
+    );
+};
