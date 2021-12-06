6c6c1093cafb559e4cdea1acda250af382e3f445
IlliaLisitsa <liyalisica9356@gmail.com>
2021-12-06 18:06:23 +0300

    add post component

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,10 +1,40 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image, TouchableOpacity } from 'react-native';
+
+import { CommentsIcon } from '../../../assets/componentIcons/comments/CommentsIcon';
+import { styles } from './postStyles';
+import { EditPostIcon } from '../../../assets/componentIcons/EditPost/EditPostIcon';
 
 export const Post = () => {
     return (
-        <View>
-            <Text>POST</Text>
+        <View style={styles.postBox}>
+            <View style={styles.postInformation}>
+                <View style={styles.userInformationBox}>
+                    <View style={styles.userIcon}>
+                        <Text style={styles.userIconText}>VG</Text>
+                    </View>
+                    <Text style={styles.userInformation}>Vitaliy Gan</Text>
+                </View>
+                <Text style={styles.postTitle}>Title</Text>
+                <Text style={styles.postBody}>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula
+                    eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient
+                    montes, nascetur ridiculus mus. Donec qu
+                </Text>
+            </View>
+            <Image
+                style={styles.postBackground}
+                source={require('../../../assets/images/PostBG.png')}
+            />
+            <TouchableOpacity style={styles.EditPostIcon} onPress={() => console.log('edit post')}>
+                <EditPostIcon />
+            </TouchableOpacity>
+            <TouchableOpacity
+                style={styles.postCommentsIcon}
+                onPress={() => console.log('comments')}
+            >
+                <CommentsIcon />
+            </TouchableOpacity>
         </View>
     );
 };
