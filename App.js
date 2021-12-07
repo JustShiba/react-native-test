0aaecd92a08bd0881cd51aea130995be4fa8b4e6
LisichkaSestrichka <79520443+LisichkaSestrichka@users.noreply.github.com>
2021-12-08 02:42:38 +0300

    add index file and provider

--- App.js
+++ App.js
@@ -1,51 +1,16 @@
-import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
-import AppLoading from 'expo-app-loading';
+import React from 'react';
+import { View } from 'react-native';
+import { Provider } from 'react-redux';
 
-import { LoginPage } from './src/pages/LoginPage/LoginPage';
-import { SignupPage } from './src/pages/SignupPage/SignupPage';
-import { useFonts } from './src/hooks/useFonts';
-import { Navigation } from './src/Navigation/Navigation';
+import { Index } from './src/components/index';
+import { store } from './src/redux/rootReducer/rootReducer';
 
 export default function App() {
-    const [IsReadyFonts, SetIsReadyFonts] = useState(false);
-    const [autorized, setAuthorized] = useState(false); //'false' value is for develop
-    const LoadFonts = async () => {
-        await useFonts();
-    };
-
-    if (!IsReadyFonts) {
-        return (
-            <AppLoading
-                startAsync={LoadFonts}
-                onFinish={() => SetIsReadyFonts(true)}
-                onError={() => {}}
-            />
-        );
-    }
-
-    if (autorized) {
-        return (
-            <View style={styles.container}>
-                <StatusBar backgroundColor="transparent" />
-                <LoginPage />
-                <SignupPage />
-            </View>
-        );
-    }
-
     return (
-        <View style={styles.container}>
-            <StatusBar backgroundColor="transparent" />
-            <Navigation />
-        </View>
+        <Provider store={store}>
+            <Index />
+        </Provider>
     );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        backgroundColor: '#F5F6F8',
-        height: '100%',
-    },
-});
+
