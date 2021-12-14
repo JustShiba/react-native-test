7ba908a25a3c3869de9eb0df148017a7d2d98892
IlliaLisitsa <liyalisica9356@gmail.com>
2021-12-14 20:49:24 +0300

    add ModalComments

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,40 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, View, Image, TouchableOpacity } from 'react-native';
 
 import { CommentsIcon } from '../../../assets/componentIcons/comments/CommentsIcon';
 import { styles } from './postStyles';
-import { EditPostIcon } from '../../../assets/componentIcons/EditPost/EditPostIcon';
+import { EditPostIcon } from '../../../assets/componentIcons/editPost/EditPostIcon';
+import { ModalComments } from '../ModalComments/ModalComments';
 
-export const Post = () => {
+export const Post = ({ postInformation, userName }) => {
+    const { body, title, comments, nickname } = postInformation.item;
+    const [modalVisible, setModalVisible] = useState(false);
+
     return (
         <View style={styles.postBox}>
             <View style={styles.postInformation}>
                 <View style={styles.userInformationBox}>
                     <View style={styles.userIcon}>
-                        <Text style={styles.userIconText}>VG</Text>
+                        <Text style={styles.userIconText}>
+                            {userName
+                                ? userName.substring(0, 2).toUpperCase()
+                                : nickname
+                                ? nickname.substring(0, 2).toUpperCase()
+                                : 'HI'}
+                        </Text>
                     </View>
-                    <Text style={styles.userInformation}>Vitaliy Gan</Text>
+                    <Text style={styles.userInformation}>{userName || nickname || 'No name'}</Text>
                 </View>
-                <Text style={styles.postTitle}>Title</Text>
-                <Text style={styles.postBody}>
-                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula
-                    eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient
-                    montes, nascetur ridiculus mus. Donec qu
-                </Text>
+                <Text style={styles.postTitle}>{title}</Text>
+                <Text style={styles.postBody}>{body}</Text>
             </View>
             <Image
                 style={styles.postBackground}
                 source={require('../../../assets/images/PostBG.png')}
             />
-            <TouchableOpacity style={styles.EditPostIcon} onPress={() => console.log('edit post')}>
+            <TouchableOpacity
+                style={styles.EditPostIcon}
+                // onPress={}
+            >
                 <EditPostIcon />
             </TouchableOpacity>
             <TouchableOpacity
                 style={styles.postCommentsIcon}
-                onPress={() => console.log('comments')}
+                onPress={() => setModalVisible(!modalVisible)}
             >
                 <CommentsIcon />
             </TouchableOpacity>
+            {modalVisible ? (
+                <ModalComments
+                    modalVisible={modalVisible}
+                    setModalVisible={setModalVisible}
+                    commentsInformation={comments}
+                />
+            ) : null}
         </View>
     );
 };
