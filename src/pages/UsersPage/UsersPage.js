e0de4c22218a3c7ba344712f63eddbedae3ab1fe
IlliaLisitsa <liyalisica9356@gmail.com>
2021-12-14 20:53:02 +0300

    add information about users

--- src/pages/UsersPage/UsersPage.js
+++ src/pages/UsersPage/UsersPage.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import { ScrollView } from 'react-native';
+import React, { useEffect } from 'react';
+import { FlatList, Text, View } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
+
 import { User } from '../../components/User/User';
+import { getAllUsersStart } from '../../redux/users/usersReducer';
 
 export const UsersPage = () => {
+    const { allUsers, loadingUsers } = useSelector((state) => state.users);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getAllUsersStart());
+    }, [dispatch]);
+
     return (
-        <ScrollView>
-            <User />
-            <User />
-            <User />
-        </ScrollView>
+        <View>
+            {loadingUsers ? (
+                <Text>loading</Text>
+            ) : (
+                <FlatList
+                    data={allUsers}
+                    renderItem={(user) => <User userInformation={user} />}
+                    keyExtractor={(user) => user.userId}
+                />
+            )}
+        </View>
     );
 };
