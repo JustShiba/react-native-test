40ff4135e514b79eadd9e6e050a2c13da48e0c0a
IlliaLisitsa <liyalisica9356@gmail.com>
2021-12-14 20:55:28 +0300

    add getting information about all users

--- src/redux/users/usersSaga.js
+++ src/redux/users/usersSaga.js
@@ -1,17 +1,34 @@
 import { select, call, put } from 'redux-saga/effects';
 
 import { apiCall } from '../../services/service';
-import { getUserFailure, getUserSuccess } from './usersReducer';
+import {
+    getUserSuccess,
+    getUserFailure,
+    getAllUsersSuccess,
+    getAllUsersFailure,
+} from './usersReducer';
 
 export function* getUserSaga() {
-    const { selectedUserId } = yield select(state => state.users)
+    const { selectedUserId } = yield select((state) => state.users);
     try {
         const response = yield call(apiCall, [`get`, `users/${selectedUserId}`]);
 
         if (response.status === 200) {
-            yield put(getUserSuccess(response.data))
+            yield put(getUserSuccess(response.data));
         }
     } catch (error) {
-        yield put(getUserFailure(error.message))
+        yield put(getUserFailure(error.message));
+    }
+}
+
+export function* getAllUsersSaga() {
+    try {
+        const response = yield call(apiCall, [`get`, `users`]);
+
+        if (response.status === 200) {
+            yield put(getAllUsersSuccess(response.data));
+        }
+    } catch (error) {
+        put(getAllUsersFailure(error.message));
     }
 }
