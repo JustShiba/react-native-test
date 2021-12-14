c8eaf916eb30ed624deaefc91108d1221b9e4527
IlliaLisitsa <liyalisica9356@gmail.com>
2021-12-14 20:52:24 +0300

    add information about posts

--- src/pages/PostsPage/PostsPage.js
+++ src/pages/PostsPage/PostsPage.js
@@ -1,16 +1,30 @@
-import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import React, { useEffect } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { Post } from '../../components/Post/Post';
+import { getAllPostsStart } from '../../redux/posts/postsReducer';
+import { getAllUsersStart } from '../../redux/users/usersReducer';
 
 export const PostsPage = () => {
+    const dispatch = useDispatch();
+    const { allPosts, loadingPosts } = useSelector((state) => state.posts);
+    useEffect(() => {
+        dispatch(getAllUsersStart());
+        dispatch(getAllPostsStart());
+    }, [dispatch]);
     return (
-        <ScrollView style={styles.allPostsBox}>
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-        </ScrollView>
+        <View>
+            {loadingPosts ? (
+                <Text>Loading</Text>
+            ) : (
+                <FlatList
+                    data={allPosts}
+                    renderItem={(post) => <Post postInformation={post} />}
+                    keyExtractor={(post) => post.postId}
+                />
+            )}
+        </View>
     );
 };
 
